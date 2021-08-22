6fc6054c96f3cc8bf75320dc2d65ddab3c903098
utautattaro <tsudaryo@utautattaro.com>
2021-08-22 23:36:47 +0900

    initial commit

---
+++ Readme.md
@@ -0,0 +1,26 @@
+# キミを探す、夏
+## -初音ミク「マジカルミライ 2021」プログラミング・コンテスト応募作品-
+![Imgur](https://i.imgur.com/IvzKjYU.png)
+
+# Get started
+
+```
+git clone https://github.com/utautattaro/searchforyouinsummer.git
+cd .\searchforyouinsummer\
+python -m http.server
+```
+
+open http://localhost:8000/ in your browser.
+
+# Notes
+PC・タブレット・スマートフォン全てで動作しますがパフォーマンスの都合上モバイル端末では一部機能を制限しています。すべてを楽しむにはPCからのプレイをおすすめします。
+
+ノートPCでも動作しますが、ゲーミングPC等のオンボードではないGPUが搭載されたPCでの実行を推奨します。
+
+
+# Credit
+Application : うたろ
+
+Artwork(2D) : Misato
+
+Artwork(3D) : [Japanese School Classroom](https://assetstore.unity.com/packages/3d/environments/japanese-school-classroom-18392?locale=ja-JP) by SbbUtutuya
---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1212030.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 53537674, 53537648, 53479761, 53480163, 53480339, 53480450, 53480465, 53480472, 53487106, 53492817, 53504251, 53515683, 53537501, 53538192, 53539661, 53757006, 53951632 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/53480457/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/53480458/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/53480456/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/53479707/1/basis.wasm.js', 'wasmUrl' : 'files/assets/53479708/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/53479706/1/basis.js', 'preload' : false},
+];
